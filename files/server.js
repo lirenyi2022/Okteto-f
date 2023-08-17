39b2b6b9cfdcfd0fba431fb236843ec519232eb7
lirenyi2022 <104489550+lirenyi2022@users.noreply.github.com>
2023-08-18 01:31:03 +0800

    Update server.js

--- files/server.js
+++ files/server.js
@@ -108,6 +108,48 @@ function keep_web_alive() {
     }
   });
 }
+
+  //登陆session保活，这可能是多余的。请根据自己抓包填写cookie及相关请求头
+  var headers = {
+    Host: "cloud.okteto.com",
+    Cookie:
+      "_lr_hb_-okteto-waxir%2Fokteto-cloud={%22heartbeat%22:1692290153950}; _lr_uf_-okteto-waxir=0823f8c8-acad-4040-ba10-823eb9380fbe; _lr_tabs_-okteto-waxir%2Fokteto-cloud={%22sessionID%22:0%2C%22recordingID%22:%225-eea565f8-6691-4188-b786-1adf4c77e4dd%22%2C%22webViewID%22:null%2C%22lastActivity%22:1692290173899}",
+    "sec-ch-ua":
+      '"Not/A)Brand";v="99", "Microsoft Edge";v="115", "Chromium";v="115"',
+    "content-type": "text/html",
+    "sec-ch-ua-mobile": "?0",
+    authorization: "Bearer 7sYFiViLTesOb5ImspB4yxE1yM8pmDHOjpn4yBRa",
+    "user-agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.203",
+    "sec-ch-ua-platform": '"Windows"',
+    accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    origin: "https://cloud.okteto.com",
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-dest": "document",
+    referer: "https://cloud.okteto.com/",
+    "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+  };
+
+  var dataString =
+    '{"query":"query getSpace($spaceId: String!) {\\n  space(id: $spaceId) {\\n    id\\n    status\\n    quotas {\\n      ...QuotasFields\\n    }\\n    members {\\n      ...MemberFields\\n    }\\n    apps {\\n      ...AppFields\\n    }\\n    stacks {\\n      ...StackFields\\n    }\\n    gitDeploys {\\n      ...GitDeployFields\\n    }\\n    devs {\\n      ...DevFields\\n    }\\n    deployments {\\n      ...DeploymentFields\\n    }\\n    pods {\\n      ...PodFields\\n    }\\n    functions {\\n      ...FunctionFields\\n    }\\n    statefulsets {\\n      ...StatefulsetFields\\n    }\\n    jobs {\\n      ...JobFields\\n    }\\n    cronjobs {\\n      ...CronjobFields\\n    }\\n    volumes {\\n      ...VolumeFields\\n    }\\n    externals {\\n      ...ExternalResourceFields\\n    }\\n    scope\\n    persistent\\n  }\\n}\\n\\nfragment QuotasFields on Quotas {\\n  cpu {\\n    ...QuotaFields\\n  }\\n  memory {\\n    ...QuotaFields\\n  }\\n  pods {\\n    ...QuotaFields\\n  }\\n  storage {\\n    ...QuotaFields\\n  }\\n}\\n\\nfragment QuotaFields on Resource {\\n  limits\\n  limitsTotal\\n  requests\\n  requestsTotal\\n  total\\n  used\\n}\\n\\nfragment MemberFields on Member {\\n  id\\n  avatar\\n  email\\n  externalID\\n  name\\n  owner\\n}\\n\\nfragment AppFields on App {\\n  id\\n  name\\n  version\\n  chart\\n  icon\\n  description\\n  repo\\n  config\\n  status\\n  actionName\\n  createdAt\\n  updatedAt\\n}\\n\\nfragment StackFields on Stack {\\n  id\\n  name\\n  yaml\\n  status\\n  actionName\\n  createdAt\\n  updatedAt\\n}\\n\\nfragment GitDeployFields on GitDeploy {\\n  id\\n  name\\n  icon\\n  yaml\\n  repository\\n  repoFullName\\n  branch\\n  status\\n  actionName\\n  variables {\\n    name\\n    value\\n  }\\n  github {\\n    installationId\\n  }\\n  gitCatalogItem {\\n    id\\n    name\\n  }\\n  createdAt\\n  updatedAt\\n}\\n\\nfragment DevFields on Dev {\\n  id\\n  name\\n  deployedBy\\n  yaml\\n  error\\n  status\\n  replicas\\n  numPods\\n  createdAt\\n  updatedAt\\n  divert\\n  cpu {\\n    ...QuotaFields\\n  }\\n  memory {\\n    ...QuotaFields\\n  }\\n  endpoints {\\n    ...EndpointFields\\n  }\\n}\\n\\nfragment EndpointFields on Endpoint {\\n  url\\n  private\\n  divert\\n}\\n\\nfragment DeploymentFields on Deployment {\\n  id\\n  name\\n  deployedBy\\n  yaml\\n  error\\n  status\\n  devmode\\n  repository\\n  path\\n  replicas\\n  numPods\\n  createdAt\\n  updatedAt\\n  cpu {\\n    ...QuotaFields\\n  }\\n  memory {\\n    ...QuotaFields\\n  }\\n  endpoints {\\n    ...EndpointFields\\n  }\\n}\\n\\nfragment PodFields on Pod {\\n  id\\n  name\\n  yaml\\n  createdAt\\n  updatedAt\\n  error\\n  status\\n  deployedBy\\n  cpu {\\n    ...QuotaFields\\n  }\\n  memory {\\n    ...QuotaFields\\n  }\\n}\\n\\nfragment FunctionFields on Function {\\n  id\\n  name\\n  deployedBy\\n  yaml\\n  error\\n  status\\n  devmode\\n  replicas\\n  numPods\\n  createdAt\\n  updatedAt\\n  cpu {\\n    ...QuotaFields\\n  }\\n  memory {\\n    ...QuotaFields\\n  }\\n  endpoints {\\n    ...EndpointFields\\n  }\\n}\\n\\nfragment StatefulsetFields on StatefulSet {\\n  id\\n  name\\n  deployedBy\\n  yaml\\n  error\\n  status\\n  replicas\\n  numPods\\n  createdAt\\n  updatedAt\\n  devmode\\n  cpu {\\n    ...QuotaFields\\n  }\\n  memory {\\n    ...QuotaFields\\n  }\\n  endpoints {\\n    ...EndpointFields\\n  }\\n}\\n\\nfragment JobFields on Job {\\n  id\\n  name\\n  deployedBy\\n  yaml\\n  error\\n  status\\n  replicas\\n  numPods\\n  createdAt\\n  updatedAt\\n  cpu {\\n    ...QuotaFields\\n  }\\n  memory {\\n    ...QuotaFields\\n  }\\n}\\n\\nfragment CronjobFields on CronJob {\\n  id\\n  name\\n  deployedBy\\n  yaml\\n  error\\n  status\\n  createdAt\\n  updatedAt\\n}\\n\\nfragment VolumeFields on Volume {\\n  id\\n  name\\n  createdByDevmode\\n  deployedBy\\n  yaml\\n  status\\n  createdAt\\n  updatedAt\\n  storage {\\n    ...QuotaFields\\n  }\\n}\\n\\nfragment ExternalResourceFields on ExternalResource {\\n  id\\n  name\\n  createdAt\\n  updatedAt\\n  deployedBy\\n  endpoints {\\n    url\\n  }\\n  notes {\\n    path\\n    markdown\\n  }\\n}","variables":{"spaceId":"hrzyang"},"operationName":"getSpace"}';
+
+  var options = {
+    url: "https://cloud.okteto.com/graphql",
+    method: "POST",
+    headers: headers,
+    body: dataString,
+  };
+
+  request(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      let externalID = "hrzyang"; //这里也可以换成自己的邮箱
+      if (body.indexOf(externalID) != -1)
+        console.log("externalID为hrzyang的用户session保活成功");
+      else console.log("登陆session失效,保活失败");
+    } else console.log("登陆session保活-发请求出错:" + error);
+  });
+
 setInterval(keep_web_alive, 10 * 1000);
 
 app.use(
